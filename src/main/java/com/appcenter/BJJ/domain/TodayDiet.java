044766bebdeeb762481bc82551b2d38e7958f3ac
KEISS <86196038+ImKEISS@users.noreply.github.com>
2024-08-27 22:10:42 +0900

    [Feat] 메뉴 크롤링 기능 추가

---
+++ src/main/java/com/appcenter/BJJ/domain/TodayDiet.java
@@ -0,0 +1,57 @@
+package com.appcenter.BJJ.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Getter
+@Entity
+@Table(name = "today_diet_tb")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class TodayDiet {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String price;
+
+    private String kcal;
+
+    private LocalDate date;
+
+    private Long mainMenuId;
+
+    private Long subMenuId;
+
+    private String restMenu;
+
+    @Builder
+    private TodayDiet(String price, String kcal, LocalDate date) {
+        this.price = price;
+        this.kcal = kcal;
+        this.date = date;
+    }
+
+    public void determineMenu(Long mainMenuId) {
+        this.mainMenuId = mainMenuId;
+        this.subMenuId = null;
+        this.restMenu = null;
+    }
+
+    public void determineMenu(Long mainMenuId, Long subMenuId) {
+        this.mainMenuId = mainMenuId;
+        this.subMenuId = subMenuId;
+        this.restMenu = null;
+    }
+
+    public void determineMenu(Long mainMenuId, Long subMenuId, String restMenu) {
+        this.mainMenuId = mainMenuId;
+        this.subMenuId = subMenuId;
+        this.restMenu = restMenu;
+    }
+}
---
+++ src/main/java/com/appcenter/BJJ/repository/TodayDietRepository.java
@@ -0,0 +1,7 @@
+package com.appcenter.BJJ.repository;
+
+import com.appcenter.BJJ.domain.TodayDiet;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TodayDietRepository extends JpaRepository<TodayDiet, Long> {
+}
