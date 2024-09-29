2cc8bbe866bfb81ea512e952ee84b7657fe2f312
jiyunio <jiyun3269@naver.com>
2024-09-30 04:18:03 +0900

    [Chore] 서버 포트 추가

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,5 @@
 FROM openjdk:17-jdk-slim
 LABEL maintainer="bjj"
+EXPOSE &{{ secrets.SERVER_PORT }}
 COPY ./build/libs/BJJ-0.0.1-SNAPSHOT.jar app.jar
 ENTRYPOINT ["java","-jar","/app.jar"]
--- src/main/java/com/appcenter/BJJ/BjjApplication.java
+++ src/main/java/com/appcenter/BJJ/BjjApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BjjApplication {
-	public static void main(String[] args) {
-		SpringApplication.run(BjjApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BjjApplication.class, args);
+    }
 }
