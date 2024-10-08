6a2b96a080f9a34397fff799f0ec660e88066cc0
jiyunio <jiyun3269@naver.com>
2024-10-09 00:00:43 +0900

    [Feat] nickname 중복 여부 확인 및 변경 기능 추가

---
+++ src/main/java/com/appcenter/BJJ/domain/member/controller/MemberController.java
@@ -0,0 +1,64 @@
+package com.appcenter.BJJ.domain.member.controller;
+
+import com.appcenter.BJJ.domain.member.service.MemberService;
+import com.appcenter.BJJ.domain.member.dto.MemberRes;
+import com.appcenter.BJJ.domain.member.dto.SignupReq;
+import com.appcenter.BJJ.global.exception.CustomException;
+import com.appcenter.BJJ.global.exception.ErrorCode;
+import com.appcenter.BJJ.global.jwt.UserDetailsImpl;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Slf4j
+@RestController
+@RequestMapping("/api/member")
+@RequiredArgsConstructor
+@Tag(name = "Member", description = "회원 API")
+public class MemberController {
+    private final MemberService memberService;
+
+    @Operation(summary = "회원가입",
+            description = """
+                    - 소셜로그인 처음 이용시 연결\s
+                    - requestDTO : SignupDTO\s""")
+    @PostMapping("/sign-up")
+    public ResponseEntity<Map<String, String>> signUp(@RequestBody SignupReq signupReq) {
+        log.info("MemberController.signUp() - 진입");
+        Map<String, String> signupRes = new HashMap<>();
+        signupRes.put("token", memberService.signUp(signupReq));
+        log.info("MemberController.signUp() - 회원가입 성공");
+        return ResponseEntity.ok(signupRes);
+    }
+
+    @Operation(summary = "모든 회원 조회")
+    @GetMapping("")
+    public ResponseEntity<MemberRes> getMember(@AuthenticationPrincipal UserDetailsImpl userDetails) {
+        log.info("MemberController.getMember() - 진입");
+        return ResponseEntity.ok(memberService.getMember(userDetails.member().getId()));
+    }
+
+    @Operation(summary = "닉네임 중복 확인")
+    @PostMapping("/check-nickname")
+    public ResponseEntity<Boolean> checkNickname(@RequestParam String nickname) {
+        if (memberService.isNicknameAvailable(nickname)) return ResponseEntity.ok(true);
+        else {
+            throw new CustomException(ErrorCode.NICKNAME_ALREADY_REGISTERED);
+        }
+    }
+
+    @Operation(summary = "닉네임 수정")
+    @PatchMapping("/nickname")
+    public ResponseEntity<Map<String, String>> putNickname(@AuthenticationPrincipal UserDetailsImpl userDetails, @RequestParam String nickname) {
+        Map<String, String> nicknameRes = new HashMap<>();
+        nicknameRes.put("nickname", memberService.changeNickname(userDetails.getNickname(), nickname));
+        return ResponseEntity.ok(nicknameRes);
+    }
+}
--- src/main/java/com/appcenter/BJJ/domain/member/service/MemberService.java
+++ src/main/java/com/appcenter/BJJ/domain/member/service/MemberService.java
@@ -1,12 +1,12 @@
-package com.appcenter.BJJ.service;
+package com.appcenter.BJJ.domain.member.service;
 
-import com.appcenter.BJJ.domain.Member;
-import com.appcenter.BJJ.dto.MemberRes;
-import com.appcenter.BJJ.dto.SignupReq;
-import com.appcenter.BJJ.exception.CustomException;
-import com.appcenter.BJJ.exception.ErrorCode;
-import com.appcenter.BJJ.jwt.JwtProvider;
-import com.appcenter.BJJ.repository.MemberRepository;
+import com.appcenter.BJJ.domain.member.domain.Member;
+import com.appcenter.BJJ.domain.member.dto.MemberRes;
+import com.appcenter.BJJ.domain.member.dto.SignupReq;
+import com.appcenter.BJJ.domain.member.repository.MemberRepository;
+import com.appcenter.BJJ.global.exception.CustomException;
+import com.appcenter.BJJ.global.exception.ErrorCode;
+import com.appcenter.BJJ.global.jwt.JwtProvider;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -23,15 +23,15 @@ public class MemberService {
     private final JwtProvider jwtProvider;
 
     public String signUp(SignupReq signupReq) {
-        log.info("MemberService-signup: 진입");
+        log.info("MemberService.signup() - 진입");
         Member member = memberRepository.findByEmailAndProvider(signupReq.getEmail(), signupReq.getProvider()).orElseThrow(
                 () -> new CustomException(ErrorCode.USER_NOT_FOUND)
         );
-        member.update("ROLE_USER");
+        member.updateMemberInfo(signupReq.getNickname(), "ROLE_USER");
         memberRepository.save(member);
-        log.info("MemberService-signup: ROLE_USER로 변경 완료 및 회원가입 성공");
+        log.info("MemberService.signup() - ROLE_USER로 변경 완료 및 회원가입 성공");
         String accessToken = getToken(member.getProviderId(), JwtProvider.validAccessTime);
-        log.info("MemberService-signup: 토큰 발급 성공");
+        log.info("MemberService.signup() - 토큰 발급 성공");
         return accessToken;
     }
 
@@ -39,12 +39,28 @@ public class MemberService {
         Member member = memberRepository.findById(id).orElseThrow(
                 () -> new CustomException(ErrorCode.INVALID_CREDENTIALS)
         );
-        log.info("MemberService-getMember: 회원 정보 조회 성공");
+        log.info("MemberService.getMember() - 회원 정보 조회 성공");
         return MemberRes.builder()
                 .nickname(member.getNickname())
                 .email(member.getEmail())
                 .provider(member.getProvider())
                 .build();
+    }
+
+    public boolean isNicknameAvailable(String nickname) {
+        if (memberRepository.existsByNickname(nickname)) {
+            return true;
+        }
+        throw new CustomException(ErrorCode.NICKNAME_ALREADY_REGISTERED);
+    }
+
+    public String changeNickname(String currentNickname, String newNickname) {
+        Member member = memberRepository.findByNickname(currentNickname).orElseThrow(
+                () -> new CustomException(ErrorCode.USER_NOT_FOUND)
+        );
+        member.updateNickname(newNickname);
+        memberRepository.save(member);
+        return newNickname;
     }
 
     public String getToken(String providerId, Long time) {
