a9cc764b74177f43aa7eb818c4b604f2bd909caa
jiyunio <jiyun3269@naver.com>
2024-10-09 01:37:53 +0900

    [Test] userdetailsimpl getUsername의 return 값 변경

--- src/main/java/com/appcenter/BJJ/domain/member/service/MemberService.java
+++ src/main/java/com/appcenter/BJJ/domain/member/service/MemberService.java
@@ -13,10 +13,12 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class MemberService {
     private final MemberRepository memberRepository;
     private final AuthenticationManager authenticationManager;
--- src/main/java/com/appcenter/BJJ/global/oauth/OAuth2SuccessHandler.java
+++ src/main/java/com/appcenter/BJJ/global/oauth/OAuth2SuccessHandler.java
@@ -2,7 +2,6 @@ package com.appcenter.BJJ.global.oauth;
 
 import com.appcenter.BJJ.global.jwt.JwtProvider;
 import com.appcenter.BJJ.global.jwt.UserDetailsImpl;
-import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
@@ -33,11 +32,12 @@ public class OAuth2SuccessHandler implements AuthenticationSuccessHandler {
         String token = jwtProvider.generateToken(authentication, JwtProvider.validAccessTime);
         String redirectUrl;
         if (userDetails.getMember().getRole().equals("ROLE_GUEST")) {
-            log.info("OAuth2SuccessHandler-onAuthenticationSuccess: 회원가입으로 이동");
+            log.info("OAuth2SuccessHandler.onAuthenticationSuccess() - 회원가입으로 이동");
             redirectUrl = UriComponentsBuilder.fromHttpUrl(signUpUrl)
+                    .queryParam("email", userDetails.getMember().getEmail())
                     .queryParam("token", token).toUriString();
         } else {
-            log.info("OAuth2SuccessHandler-onAuthenticationSuccess: 로그인으로 이동");
+            log.info("OAuth2SuccessHandler.onAuthenticationSuccess() - 로그인으로 이동");
             redirectUrl = UriComponentsBuilder.fromHttpUrl(signInUrl)
                     .queryParam("token", token).toUriString();
         }
