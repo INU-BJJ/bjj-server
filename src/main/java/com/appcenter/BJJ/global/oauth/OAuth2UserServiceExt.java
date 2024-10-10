9a373aeb1729fde112abf4ba076b8808ff740274
jiyunio <jiyun3269@naver.com>
2024-10-10 18:17:36 +0900

    [Test] token 발급 기간 수정

--- src/main/java/com/appcenter/BJJ/global/oauth/OAuth2UserServiceExt.java
+++ src/main/java/com/appcenter/BJJ/global/oauth/OAuth2UserServiceExt.java
@@ -10,6 +10,7 @@ import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
+import io.jsonwebtoken.impl.DefaultJwtParser;
 
 @Slf4j
 @Service
