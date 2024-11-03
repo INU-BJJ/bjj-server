c3d5a4b70634501971ac2a0d9c609ae1a9568b0f
jiyunio <jiyun3269@naver.com>
2024-11-03 23:48:42 +0900

    [Refactor] 상속 filter 변경

--- src/main/java/com/appcenter/BJJ/global/jwt/JwtFilter.java
+++ src/main/java/com/appcenter/BJJ/global/jwt/JwtFilter.java
@@ -2,27 +2,29 @@ package com.appcenter.BJJ.global.jwt;
 
 import jakarta.servlet.*;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
 @Slf4j
 @RequiredArgsConstructor
-public class JwtFilter extends GenericFilter {
+public class JwtFilter extends OncePerRequestFilter {
     private final JwtProvider jwtProvider;
 
     @Override
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         String token = getToken((HttpServletRequest) request);
         if (jwtProvider.validateToken(token)) {
             log.info("JwtFilter.doFilter() - token 있음 & 인증 시작");
             Authentication authentication = jwtProvider.getAuthentication(token);
             SecurityContextHolder.getContext().setAuthentication(authentication);
         }
-        chain.doFilter(request, response);
+        filterChain.doFilter(request, response);
     }
 
     public String getToken(HttpServletRequest request) {
