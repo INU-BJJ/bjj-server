64c166ba9248bc9d66648059a30d603ad3fbfa75
jiyunio <jiyun3269@naver.com>
2024-11-05 16:58:32 +0900

    [Feat] 아이템 관련 에러코드 추가

--- src/main/java/com/appcenter/BJJ/global/exception/ErrorCode.java
+++ src/main/java/com/appcenter/BJJ/global/exception/ErrorCode.java
@@ -7,22 +7,22 @@ import org.springframework.http.HttpStatus;
 @Getter
 @RequiredArgsConstructor
 public enum ErrorCode {
-    //400 Bad Request 서버 이해 못해
+    //400 Bad Request
     INVALID_CREDENTIALS(HttpStatus.BAD_REQUEST, "이메일 및 비밀번호가 맞지 않습니다."),
     PASSWORD_MISMATCH(HttpStatus.BAD_REQUEST, "비밀번호가 맞지 않습니다."),
 
-    //401 Unauthorized 클라이언트 인증
+    //401 Unauthorized
     ERROR_UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "사용자가 인증되지 않았습니다."),
     INVALID_TOKEN_FORMAT(HttpStatus.UNAUTHORIZED, "토큰의 형식이 유효하지 않습니다."),
     EXPIRED_TOKEN(HttpStatus.UNAUTHORIZED, "토큰 기간이 만료됐습니다."),
 
-    //403 Forbidden 클라이언트는 아는데 접근 못해
 
-
-    //404 Not Found 리소스를 찾을 수 없어
+    //404 Not Found
     USER_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 회원이 존재하지 않습니다."),
 
-    //409 Conflict 중복됐어
+    ITEM_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 아이템이 존재하지 않습니다."),
+
+    //409 Conflict
     EMAIL_ALREADY_REGISTERED(HttpStatus.CONFLICT, "이미 등록된 이메일입니다."),
     NICKNAME_ALREADY_REGISTERED(HttpStatus.CONFLICT, "이미 등록된 닉네임입니다."),
     ERROR_SOCIAL_LOGIN_CONFLICT(HttpStatus.CONFLICT, "해당 소셜 계정은 이미 다른 계정에 연결되어 있습니다.");
