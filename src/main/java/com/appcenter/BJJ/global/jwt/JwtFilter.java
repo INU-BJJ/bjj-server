ade6b669af327db748a3840c50e4091355676c41
jiyunio <jiyun3269@naver.com>
2024-11-11 18:12:36 +0900

    [Refactor] 토큰 null값 검사 코드 추가

--- src/main/java/com/appcenter/BJJ/global/jwt/JwtFilter.java
+++ src/main/java/com/appcenter/BJJ/global/jwt/JwtFilter.java
@@ -19,7 +19,7 @@ public class JwtFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         String token = getToken((HttpServletRequest) request);
-        if (jwtProvider.validateToken(token)) {
+        if (token != null && jwtProvider.validateToken(token)) {
             log.info("JwtFilter.doFilter() - token 있음 & 인증 시작");
             Authentication authentication = jwtProvider.getAuthentication(token);
             SecurityContextHolder.getContext().setAuthentication(authentication);
