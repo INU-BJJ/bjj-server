af0f90cb1a77f229a5fca31f71839c05c048bafd
jiyunio <jiyun3269@naver.com>
2024-11-18 04:07:01 +0900

    [Feat] 쿼리문으로 JPA 메소드 정의 #4

--- src/main/java/com/appcenter/BJJ/domain/member/repository/MemberRepository.java
+++ src/main/java/com/appcenter/BJJ/domain/member/repository/MemberRepository.java
@@ -2,6 +2,7 @@ package com.appcenter.BJJ.domain.member.repository;
 
 import com.appcenter.BJJ.domain.member.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
@@ -13,6 +14,6 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
     Optional<Member> findByEmailAndProvider(String email, String provider);
 
     Optional<Member> findByNickname(String nickname);
-
+    @Query("SELECT COUNT(m) > 0 FROM Member m WHERE m.nickname = :nickname AND m.role = 'ROLE_USER'")
     boolean existsByNickname(String nickname);
 }
