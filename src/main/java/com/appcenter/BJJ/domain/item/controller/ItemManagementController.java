5a7480f7f92a0b9a89e0c89996da5ec58c569d7d
jiyunio <jiyun3269@naver.com>
2025-02-12 14:50:36 +0900

    [Style] 아이템 업로드 클래스명 변경 #39

---
+++ src/main/java/com/appcenter/BJJ/domain/item/controller/ItemManagementController.java
@@ -0,0 +1,34 @@
+package com.appcenter.BJJ.domain.item.controller;
+
+import com.appcenter.BJJ.domain.item.dto.ItemRes;
+import com.appcenter.BJJ.domain.item.service.ItemManagementService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/admin/items")
+@RequiredArgsConstructor
+@Tag(name = "Item (Admin)", description = "아이템 API (관리자만 사용)")
+public class ItemManagementController {
+
+    private final ItemManagementService itemManagementService;
+
+    @Operation(summary = "아이템 이미지 저장",
+            description = "character / backgound 로 파일명 설정하고 이용")
+    @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<List<ItemRes>> uploadItems(@RequestPart MultipartFile infoFile, @RequestPart MultipartFile imageFile) throws IOException {
+        return new ResponseEntity<>(itemManagementService.uploadItems(infoFile, imageFile), HttpStatus.CREATED);
+    }
+}
---
+++ src/main/java/com/appcenter/BJJ/domain/item/service/ItemManagementService.java
@@ -0,0 +1,96 @@
+package com.appcenter.BJJ.domain.item.service;
+
+import com.appcenter.BJJ.domain.item.domain.Item;
+import com.appcenter.BJJ.domain.item.dto.ItemRes;
+import com.appcenter.BJJ.domain.item.dto.ItemVO;
+import com.appcenter.BJJ.domain.item.enums.ItemType;
+import com.appcenter.BJJ.domain.item.repository.ItemRepository;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class ItemManagementService {
+    private final ObjectMapper objectMapper;
+    private final ItemRepository itemRepository;
+
+    @Value("${storage.images.item}")
+    private String ITEM_IMG_DIR;
+
+    public List<ItemRes> uploadItems(MultipartFile infoFile, MultipartFile zipImageFile) throws IOException {
+
+        ItemType itemType = ItemType.valueOf(Objects.requireNonNull(zipImageFile.getOriginalFilename()).split("\\.")[0].toUpperCase());
+        List<String> fileNameList = unZip(zipImageFile, itemType);
+
+        List<Item> itemList = jsonToList(infoFile).stream()
+                .map(itemVO -> Item.create(itemVO, fileNameList.get(itemVO.getItemId() - 1), itemType))
+                .toList();
+
+        itemRepository.saveAll(itemList);
+
+        return itemList.stream().map(item -> ItemRes.builder()
+                .itemId(item.getItemId())
+                .itemName(item.getItemName())
+                .itemType(item.getItemType())
+                .itemLevel(item.getItemLevel())
+                .imageName(item.getImageName())
+                .build()).collect(Collectors.toList());
+    }
+
+    private List<String> unZip(MultipartFile zipImageFile, ItemType itemType) throws IOException {
+        InputStream inputStream = zipImageFile.getInputStream();
+        ZipInputStream zipInputStream = new ZipInputStream(inputStream);
+
+        ZipEntry entry;
+        List<String> fileNameList = new ArrayList<>();
+
+        while ((entry = zipInputStream.getNextEntry()) != null) {
+            if (entry.isDirectory()) continue;
+
+            String imageFileName = entry.getName().split("/")[1];
+            fileNameList.add(imageFileName);
+
+            File imageFile = new File(ITEM_IMG_DIR + itemType + "/" + imageFileName);
+
+            //이미지 파일 write
+            try (FileOutputStream outputStream = new FileOutputStream(imageFile)) {
+                byte[] bytes = new byte[1024];
+                int length;
+                while ((length = zipInputStream.read(bytes)) >= 0) {
+                    outputStream.write(bytes, 0, length);
+                }
+            }
+        }
+        zipInputStream.closeEntry();
+
+        return fileNameList.stream()
+                // 파일의 이름을 기준으로 정렬
+                .sorted(Comparator.comparingInt(fileName -> Integer.parseInt(fileName.split("\\.")[0])))
+                .toList();
+    }
+
+    private List<ItemVO> jsonToList(MultipartFile file) throws IOException {
+        String jsonFile = new String(file.getBytes(), StandardCharsets.UTF_8);
+        return objectMapper.readValue(jsonFile, new TypeReference<>() {
+        });
+    }
+}
