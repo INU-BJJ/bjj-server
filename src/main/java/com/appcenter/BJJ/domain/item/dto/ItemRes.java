cdddc204bd50e98167bd0100efe7c302a1a90061
jiyunio <jiyun3269@naver.com>
2025-02-12 14:47:46 +0900

    [Feat] 뽑기 기능 & 중복 아이템 뽑을 시, 기간 처리 부분 수정 &  #38 #39

--- src/main/java/com/appcenter/BJJ/domain/item/dto/ItemRes.java
+++ src/main/java/com/appcenter/BJJ/domain/item/dto/ItemRes.java
@@ -2,25 +2,23 @@ package com.appcenter.BJJ.domain.item.dto;
 
 import com.appcenter.BJJ.domain.item.enums.ItemLevel;
 import com.appcenter.BJJ.domain.item.enums.ItemType;
-import lombok.Builder;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.experimental.SuperBuilder;
 
 @Getter
-@Builder
+@SuperBuilder
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
 public class ItemRes {
-    private final int itemId;
 
-    private final int price;
+    private final Integer itemId;
+
+    private final String itemName;
 
     private final ItemType itemType;
 
     private final ItemLevel itemLevel;
 
-    private final String itemName;
-
-//    private final boolean isWearing;
-//
-//    private final boolean isOwned;
-//
-//    private LocalDate validPeriod;
+    private final String imageName;
 }
--- src/main/java/com/appcenter/BJJ/domain/item/dto/ItemVO.java
+++ src/main/java/com/appcenter/BJJ/domain/item/dto/ItemVO.java
@@ -1,23 +1,17 @@
 package com.appcenter.BJJ.domain.item.dto;
 
 import com.appcenter.BJJ.domain.item.enums.ItemLevel;
-import com.appcenter.BJJ.domain.item.enums.ItemType;
-import lombok.Builder;
+import lombok.AccessLevel;
 import lombok.Getter;
-import lombok.Setter;
+import lombok.RequiredArgsConstructor;
 
 @Getter
-@Builder
+@RequiredArgsConstructor(access = AccessLevel.PRIVATE)
 public class ItemVO {
+
     private final Integer itemId;
 
-    private final int price;
+    private final String itemName;
 
     private final ItemLevel itemLevel;
-
-    @Setter
-    private ItemType itemType;
-
-    @Setter
-    private String imageName;
 }
