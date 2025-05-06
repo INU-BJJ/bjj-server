e94fe226bd7e8005a68813f2bcafce21a98055b8
jiyunio <jiyun3269@naver.com>
2025-05-06 23:47:50 +0900

    [Feat] 정지를 당한 회원에게 리뷰 작성 제재 예외 처리 추가 - 정지 시작일자와 정지 종료일자를 프론트에게 전달하고자 response에 추가 - 회원이 리뷰를 작성하기 전, 해당 회원이 리뷰 작성에 대해 정지 당한 회원인지 확인 후 작성될 수 있도록 ReviewService.create() 수정 #63

--- src/main/java/com/appcenter/BJJ/global/exception/CustomExceptionHandler.java
+++ src/main/java/com/appcenter/BJJ/global/exception/CustomExceptionHandler.java
@@ -1,5 +1,7 @@
 package com.appcenter.BJJ.global.exception;
 
+import com.appcenter.BJJ.global.exception.dto.ErrorDTO;
+import com.appcenter.BJJ.global.exception.dto.ReviewSuspensionDTO;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -16,4 +18,10 @@ public class CustomExceptionHandler {
     protected ResponseEntity<ErrorDTO> handleValidationExceptions(MethodArgumentNotValidException e) {
         return ErrorDTO.toResponseEntity(ErrorCode.INVALID_INPUT, e.getBindingResult().getFieldErrors());
     }
+
+    @ExceptionHandler(ReviewSuspensionException.class)
+    protected ResponseEntity<ReviewSuspensionDTO> handleReportException(ReviewSuspensionException e) {
+        return ReviewSuspensionDTO.toResponseEntity(ErrorCode.MEMBER_SUSPENDED_FOR_REVIEW, e.getStartAt(), e.getEndAt());
+    }
+
 }
