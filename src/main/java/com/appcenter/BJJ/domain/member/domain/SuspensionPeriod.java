a2979a98ca1fccdfc147618b20cdfe40425525c2
jiyunio <jiyun3269@naver.com>
2025-05-14 01:01:55 +0900

    [Refactor] SuspensionPeriod 객체를 builder가 아닌 생성자를 이용해 생성되도록 변경 #63

--- src/main/java/com/appcenter/BJJ/domain/member/domain/SuspensionPeriod.java
+++ src/main/java/com/appcenter/BJJ/domain/member/domain/SuspensionPeriod.java
@@ -1,26 +1,28 @@
 package com.appcenter.BJJ.domain.member.domain;
 
 import jakarta.persistence.Embeddable;
-import lombok.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
-@Embeddable
 @Getter
-@Builder
-@NoArgsConstructor
+@Embeddable
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class SuspensionPeriod {
 
     private LocalDateTime startAt;
 
     private LocalDateTime endAt;
 
-    public static SuspensionPeriod create() {
-        return SuspensionPeriod.builder()
-                .startAt(LocalDateTime.of(1970, 1, 1, 0, 0, 0, 0))
-                .endAt(LocalDateTime.of(1970, 1, 1, 0, 0, 0, 0))
-                .build();
+    public static SuspensionPeriod init() {
+        return new SuspensionPeriod(
+                LocalDateTime.of(1970, 1, 1, 0, 0, 0, 0),
+                LocalDateTime.of(1970, 1, 1, 0, 0, 0, 0)
+        );
     }
 
     public void suspend(LocalDateTime startAt, LocalDateTime endAt){
