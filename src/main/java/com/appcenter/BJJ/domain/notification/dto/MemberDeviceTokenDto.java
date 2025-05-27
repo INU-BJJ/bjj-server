f7c487614f2583a0651927c8985a8644275d63f5
KEISS <86196038+ImKEISS@users.noreply.github.com>
2025-05-27 18:32:03 +0900

    [Refactor] 불필요한 어노테이션 제거
    사용하지 않는 @ToString 어노테이션을 제거
    
    #60

--- src/main/java/com/appcenter/BJJ/domain/notification/dto/MemberDeviceTokenDto.java
+++ src/main/java/com/appcenter/BJJ/domain/notification/dto/MemberDeviceTokenDto.java
@@ -3,10 +3,8 @@ package com.appcenter.BJJ.domain.notification.dto;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.ToString;
 
 @Getter
-@ToString
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class MemberDeviceTokenDto {
     private final Long memberId;
