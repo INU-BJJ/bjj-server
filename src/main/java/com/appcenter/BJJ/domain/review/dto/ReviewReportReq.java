f1d66529665c59eced3ac6570d2490d95e4d60ec
jiyunio <jiyun3269@naver.com>
2025-05-27 13:32:46 +0900

    [Refactor] 리뷰 신고의 content를 여러 개 받을 수 있도록 리스트 형식으로 request 변경
    - 신고 내용을 각각 저장하기에 reviewId와 reviewerId가 중복으로 존재 => 중복 제거해서 신고 개수를 세도록 설정
    
    #79

--- src/main/java/com/appcenter/BJJ/domain/review/dto/ReviewReportReq.java
+++ src/main/java/com/appcenter/BJJ/domain/review/dto/ReviewReportReq.java
@@ -3,9 +3,11 @@ package com.appcenter.BJJ.domain.review.dto;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 
+import java.util.List;
+
 @Getter
 public class ReviewReportReq {
 
-    @Schema(description = "리뷰 신고 내용", example = "사진을 못 찍으셔요.")
-    private String content;
+    @Schema(description = "리뷰 신고 내용", example = "[\"메뉴와 관련없는 내용\", \"사진을 못 찍으셔요.\"]")
+    private List<String> content;
 }
