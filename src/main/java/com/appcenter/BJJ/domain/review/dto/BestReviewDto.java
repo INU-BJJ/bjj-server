1038c5c91235154d02fbaa24f5fd1224b306554a
KEISS <86196038+ImKEISS@users.noreply.github.com>
2025-06-25 17:09:52 +0900

    [Fix] QueryDSL에서 생성자에 접근할 수 있도록 접근 제어자를 Public으로 수정 #83

--- src/main/java/com/appcenter/BJJ/domain/review/dto/BestReviewDto.java
+++ src/main/java/com/appcenter/BJJ/domain/review/dto/BestReviewDto.java
@@ -1,13 +1,12 @@
 package com.appcenter.BJJ.domain.review.dto;
 
-import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.ToString;
 
 @Getter
 @ToString
-@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@AllArgsConstructor // QueryDSL은 접근제어자로 Public 필요
 public class BestReviewDto {
     private final Long menuId;
     private final Long bestReviewId;
