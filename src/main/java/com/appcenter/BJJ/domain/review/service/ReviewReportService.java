2c14e75fc97144d7ac9a907aa36c9881cb1a5ded
jiyunio <jiyun3269@naver.com>
2025-08-29 00:02:17 -0700

    [Feat] 회원 밴처리 기능 추가
    - 회원의 BAN 처리 메소드 추가
    - 회원이 신고 및 리뷰가 가능한지 체크하는 메소드 추가
    (validateReportReview(), handleReportedReview())
    
    #105

--- src/main/java/com/appcenter/BJJ/domain/review/service/ReviewReportService.java
+++ src/main/java/com/appcenter/BJJ/domain/review/service/ReviewReportService.java
@@ -3,6 +3,7 @@ package com.appcenter.BJJ.domain.review.service;
 import com.appcenter.BJJ.domain.member.MemberRepository;
 import com.appcenter.BJJ.domain.member.domain.Member;
 import com.appcenter.BJJ.domain.member.enums.MemberStatus;
+import com.appcenter.BJJ.domain.member.schedule.MemberTaskRepository;
 import com.appcenter.BJJ.domain.member.schedule.MemberTaskService;
 import com.appcenter.BJJ.domain.review.domain.ReviewReport;
 import com.appcenter.BJJ.domain.review.dto.ReviewReportReq;
@@ -27,7 +28,9 @@ public class ReviewReportService {
     private final ReviewReportRepository reviewReportRepository;
     private final MemberRepository memberRepository;
     private final MemberTaskService memberTaskService;
+    private final MemberTaskRepository memberTaskRepository;
     public static final int REPORT_COUNT = 5;
+    public static final int BAN_COUNT = 3;
 
     @Transactional
     public void reportReview(Long reporterId, Long reviewId, ReviewReportReq reviewReportReq) {
@@ -35,6 +38,19 @@ public class ReviewReportService {
                 () -> new CustomException(ErrorCode.REVIEW_NOT_FOUND)
         ).getMemberId();
 
+        //회원의 신고 체크
+        validateReportReview(reporterId, reportedId, reviewId);
+        //작성자의 리뷰 체크
+        handleReportedReview(reportedId, reviewId);
+
+        reviewReportRepository.saveAll(reviewReportReq.getContent()
+                .stream()
+                .map(content ->
+                        ReviewReport.create(reporterId, reportedId, reviewId, content))
+                .toList());
+    }
+
+    private void validateReportReview(Long reporterId, Long reportedId, Long reviewId) {
         if (Objects.equals(reporterId, reportedId)) {
             throw new CustomException(ErrorCode.CANNOT_REPORT_OWN_REVIEW);
         }
@@ -42,22 +58,25 @@ public class ReviewReportService {
         if (reviewReportRepository.existsByReviewIdAndReporterId(reviewId, reporterId)) {
             throw new CustomException(ErrorCode.DUPLICATE_REPORT);
         }
+    }
 
-        reviewReportRepository.saveAll(reviewReportReq.getContent()
-                .stream()
-                .map(content ->
-                        ReviewReport.create(reporterId, reportedId, reviewId, content))
-                .toList());
+    private void handleReportedReview(Long reportedId, Long reviewId) {
+        Member member = memberRepository.findById(reportedId).orElseThrow(
+                () -> new CustomException(ErrorCode.USER_NOT_FOUND));
 
-        if (reviewReportRepository.countReviewReportByReviewId(reviewId) >= REPORT_COUNT) { // 일정 개수의 신고가 넘으면 작성자 정지
-            Member member = memberRepository.findById(reportedId).orElseThrow(
-                    () -> new CustomException(ErrorCode.USER_NOT_FOUND));
+        //회원 밴 체크 (COMPLETE인 MemberTask가 3개 이상이면 밴 대상)
+        if(memberTaskRepository.countByMemberId(member.getId()) >= BAN_COUNT) {
+            member.updateMemberStatus(MemberStatus.BAN);
+            return;
+        }
 
+        //회원 정지 체크
+        if (reviewReportRepository.countReviewReportByReviewId(reviewId) >= REPORT_COUNT) {
             LocalDateTime now = LocalDateTime.now();
             member.updateMemberStatus(MemberStatus.SUSPENDED);
             memberTaskService.addOrUpdateTask(member.getId(), now, now.plusDays(7));
 
-            delete(reviewId); //리뷰 신고 내역 삭제
+            delete(reviewId); //리뷰 신고 내역 sofe delete
             reviewService.delete(reviewId); // 누적 신고 당한 리뷰 삭제
         }
     }
