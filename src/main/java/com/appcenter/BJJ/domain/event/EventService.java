392a4368af098f063ee395eda95c732be9a8955e
jiyunio <jiyun3269@naver.com>
2025-09-18 16:31:28 -0700

    [Refactor] 웰컴포인트 지급 서비스 트랜잭션 추가 #111

--- src/main/java/com/appcenter/BJJ/domain/event/EventService.java
+++ src/main/java/com/appcenter/BJJ/domain/event/EventService.java
@@ -3,15 +3,18 @@ package com.appcenter.BJJ.domain.event;
 import com.appcenter.BJJ.domain.member.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class EventService {
     private final EventRepository eventRepository;
     private final MemberService memberService;
 
     private final static EventType WELCOME_EVENT = EventType.WELCOME_EVENT;
 
+    @Transactional
     public boolean welcomePoint(Long memberId) {
         boolean isParticipated = eventRepository.existsByMemberIdAndEventType(memberId, WELCOME_EVENT);
         if (isParticipated) return false; // 이미 참여 완료
