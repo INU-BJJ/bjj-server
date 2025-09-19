5e6312ecc2e32a0ac6f7a4f0a81e253752e322ba
jiyunio <jiyun3269@naver.com>
2025-09-18 21:47:58 -0700

    [Refactor] 웰컴포인트 이벤트 참여 처리 로직 개선
    1. 아직 이벤트에 참여하지 않은 경우, 포인트 지급 후 true 반환
    2. 이미 이벤트에 1회 참여한 경우, 포인트 지급없이 false 반환
    3. 이벤트 참여 기록이 2회 이상 (0 또는 1이 아닌 숫자) 인 경우, 정상적으로 발생할 수 없는 상태로 간주해 서버 내부 오류로 처리하고 예외 발생
    
    #111

--- src/main/java/com/appcenter/BJJ/domain/event/EventService.java
+++ src/main/java/com/appcenter/BJJ/domain/event/EventService.java
@@ -4,9 +4,11 @@ import com.appcenter.BJJ.domain.member.MemberService;
 import com.appcenter.BJJ.global.exception.CustomException;
 import com.appcenter.BJJ.global.exception.ErrorCode;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
@@ -19,12 +21,14 @@ public class EventService {
     @Transactional
     public boolean welcomePoint(Long memberId) {
         long participatedCount = eventRepository.countAllByMemberIdAndEventType(memberId, WELCOME_EVENT);
-        if (participatedCount >= 1) {
-            throw new CustomException(ErrorCode.EVENT_ALREADY_PARTICIPATED);
+        if (participatedCount == 0) {
+            memberService.updatePoint(memberId, WELCOME_EVENT.getRewardValue());
+            eventRepository.save(Event.create(memberId, WELCOME_EVENT));
+            return true;
+        } else if (participatedCount == 1) {
+            return false;
         }
-
-        memberService.updatePoint(memberId, WELCOME_EVENT.getRewardValue());
-        eventRepository.save(Event.create(memberId, WELCOME_EVENT));
-        return true;
+        log.error("[로그] 웰컴이벤트 포인트 지급 서버 오류 발생. memberId={}, participatedCount={}", memberId, participatedCount);
+        throw new CustomException(ErrorCode.SERVER_ERROR);
     }
 }
