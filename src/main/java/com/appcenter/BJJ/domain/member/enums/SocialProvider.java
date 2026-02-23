0484a34774cf4320ceb8db036824f71076766a4d
jiyunio <jiyun3269@naver.com>
2026-02-23 23:39:06 +0900

    [Refactor] SocialProvier에 테스트용 데이터 추가

--- src/main/java/com/appcenter/BJJ/domain/member/enums/SocialProvider.java
+++ src/main/java/com/appcenter/BJJ/domain/member/enums/SocialProvider.java
@@ -10,7 +10,8 @@ public enum SocialProvider {
     KAKAO("카카오"),
     NAVER("네이버"),
     GOOGLE("구글"),
-    APPLE("애플");
+    APPLE("애플"),
+    BJJ("밥점줘"); // test용
 
     private final String description;
 }
